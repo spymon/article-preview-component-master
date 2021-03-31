52b916cdf11f9d5de8882c0f79062fe64016b8c5
Simon Žižek <spymon12@gmail.com>
2021-03-31 11:49:51 +0200

    Improved accessibility with aria and title attributes.

--- app/js/script.js
+++ app/js/script.js
@@ -5,4 +5,9 @@ console.log(btnSci)
 
 btnSci.addEventListener('click', function () {
   card.classList.toggle('active')
+  if (card.classList.contains('active')) {
+    btnSci.setAttribute('aria-expanded', 'true')
+  } else {
+    btnSci.setAttribute('aria-expanded', 'false')
+  }
 })
